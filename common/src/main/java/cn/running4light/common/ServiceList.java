a56bd2d267f728b3988810444a567a585378f39b
dandelionflying <zhuzexiong@vip.qq.com>
2021-07-12 17:11:19 +0800

    公共的枚举类--记录服务名

--- common/src/main/java/cn/running4light/common/ServiceList.java
+++ common/src/main/java/cn/running4light/common/ServiceList.java
@@ -6,6 +6,13 @@ package cn.running4light.common;
  * @createTime 2021/7/12 16:02
  */
 public enum ServiceList {
-    basis,
-    basis2;
+    basis("basis"),
+    basis2("basis2");
+    public final String value;
+    private ServiceList(String value) {
+        this.value = value;
+    }
+    public String value() {
+        return this.value;
+    }
 }
