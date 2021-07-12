258fff535ea8bab91e619ca54df71546334cbcdb
dandelionflying <zhuzexiong@vip.qq.com>
2021-07-12 14:28:38 +0800

    basis2模块用于测试

---
+++ basis2/src/main/java/cn/running4light/basis2/Basis2Application.java
@@ -0,0 +1,13 @@
+package cn.running4light.basis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Basis2Application.class, args);
+    }
+
+}
---
+++ basis2/src/test/java/cn/running4light/basis2/Basis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.running4light.basis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basis2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
