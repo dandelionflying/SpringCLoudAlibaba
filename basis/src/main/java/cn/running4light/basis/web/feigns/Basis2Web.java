a6929baa7c33147f90695b3740932376bcbc2d35
dandelionflying <zhuzexiong@vip.qq.com>
2021-07-14 17:19:19 +0800

    yaml配置方式配置feign日志级别（失效暂未解决）

--- basis/src/main/java/cn/running4light/basis/web/feigns/Basis2Web.java
+++ basis/src/main/java/cn/running4light/basis/web/feigns/Basis2Web.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @description 统一接口管理：定义某个微服务的所有接口
  * @createTime 2021/7/12 16:45
  */
-@FeignClient(value = "basis2", configuration = Basis2FeignConfiguration.class)
+//@FeignClient(value = "basis2", configuration = Basis2FeignConfiguration.class)
+@FeignClient(value = "basis2")
 public interface Basis2Web {
     @GetMapping("/basis2/findBasis2")
     String findBasis2();
